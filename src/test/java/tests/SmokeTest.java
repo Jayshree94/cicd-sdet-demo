de0a527f5f85aea54686fa4328744fd1bfedb3bd
Jayshree Dixit <jayshree@Jayshrees-MacBook-Air.local>
2026-08-15 21:40:33 +0530

    Initial CI/CD Maven TestNG project

---
+++ src/test/java/tests/SmokeTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+
+    @Test
+    public void verifyApplication() {
+
+        System.out.println("Running Smoke Test");
+
+        Assert.assertTrue(true);
+
+    }
+}
